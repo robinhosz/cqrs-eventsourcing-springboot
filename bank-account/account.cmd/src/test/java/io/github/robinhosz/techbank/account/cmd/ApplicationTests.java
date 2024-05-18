46df1ae8bf7209c42b295aad3083ec66e6104334
José Robson <joserobsonsiqueira23@hotmail.com>
2024-05-18 10:29:46 -0300

    project structure

---
+++ bank-account/account.cmd/src/test/java/io/github/robinhosz/techbank/account/cmd/ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.robinhosz.techbank.account.cmd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
