e9760a275e0b0c8a34f9d246bc90fd8fd8bb1f62
José Robson <joserobsonsiqueira23@hotmail.com>
2024-05-20 16:33:00 -0300

    feat: register commands

--- bank-account/account.cmd/src/main/java/io/github/robinhosz/techbank/account/cmd/CommandApplication.java
+++ bank-account/account.cmd/src/main/java/io/github/robinhosz/techbank/account/cmd/CommandApplication.java
@@ -1,13 +1,33 @@
 package io.github.robinhosz.techbank.account.cmd;
 
+import io.github.robinhosz.techbank.account.cmd.api.commands.*;
+import io.github.robinhosz.techbank.cqrs.core.infrastructure.CommandDispatcher;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CommandApplication {
 
+	@Autowired
+	private CommandDispatcher commandDispatcher;
+
+	@Autowired
+	private CommandHandler commandHandler;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CommandApplication.class, args);
 	}
+
+	@PostConstruct
+	public void registerHandler() {
+
+		commandDispatcher.registerHandler(OpenAccountCommand.class, commandHandler::handle);
+		commandDispatcher.registerHandler(DepositFundsCommand.class, commandHandler::handle);
+		commandDispatcher.registerHandler(WithdrawFundsCommand.class, commandHandler::handle);
+		commandDispatcher.registerHandler(CloseAccountCommand.class, commandHandler::handle);
+	}
+
 
 }
