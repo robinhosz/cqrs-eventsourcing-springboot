a9f0aacf72534b4f3671f926582b1d0fb5d30529
José Robson <joserobsonsiqueira23@hotmail.com>
2024-05-20 14:17:52 -0300

    feat: baseevent and added pom

---
+++ cqrs-es/cqrs.core/src/main/java/io/github/robinhosz/techbank/cqrs/core/events/BaseEvent.java
@@ -0,0 +1,15 @@
+package io.github.robinhosz.techbank.cqrs.core.events;
+
+import io.github.robinhosz.techbank.cqrs.core.messages.Message;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public abstract class BaseEvent extends Message {
+    private int version;
+}
