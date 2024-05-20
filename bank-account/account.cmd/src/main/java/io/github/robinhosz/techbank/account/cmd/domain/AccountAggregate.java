5a9bd7cbd228c19dc109746bb4b3a9512e308e49
José Robson <joserobsonsiqueira23@hotmail.com>
2024-05-20 15:19:29 -0300

    feat: AccountAggregate created

---
+++ bank-account/account.cmd/src/main/java/io/github/robinhosz/techbank/account/cmd/domain/AccountAggregate.java
@@ -0,0 +1,81 @@
+package io.github.robinhosz.techbank.account.cmd.domain;
+
+import io.github.robinhosz.techbank.account.cmd.api.commands.OpenAccountCommand;
+import io.github.robinhosz.techbank.account.common.events.AccountClosedEvent;
+import io.github.robinhosz.techbank.account.common.events.AccountOpenedEvent;
+import io.github.robinhosz.techbank.account.common.events.FundsDepositedEvent;
+import io.github.robinhosz.techbank.account.common.events.FundsWithdrawEvent;
+import io.github.robinhosz.techbank.cqrs.core.domain.AggregateRoot;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@NoArgsConstructor
+public class AccountAggregate extends AggregateRoot {
+    private Boolean active;
+    private double balance;
+
+    public AccountAggregate(OpenAccountCommand command) {
+        raiseEvent(AccountOpenedEvent.builder()
+                .id(command.getId())
+                .accountHolder(command.getAccountHolder())
+                .accountType(command.getAccountType())
+                .createdDate(new Date())
+                .openingBalance(command.getOpeningBalance())
+                .build());
+    }
+
+    public void apply(AccountOpenedEvent event) {
+        this.id = event.getId();
+        this.active = true;
+        this.balance = event.getOpeningBalance();
+    }
+
+    public void depositFunds(double amount) {
+        if (!this.active) {
+            throw new IllegalStateException("Account is not active");
+        }
+        if(amount <= 0) {
+            throw new IllegalStateException("Deposit amount must be greater than zero");
+        }
+        raiseEvent(FundsDepositedEvent.builder()
+                .id(this.id)
+                .amount(amount)
+                .build());
+    }
+
+    public void apply(FundsDepositedEvent event) {
+        this.id = event.getId();
+        this.balance += event.getAmount();
+    }
+
+    public void withdrawFunds(double amount) {
+        if (!this.active) {
+            throw new IllegalStateException("Account is not active");
+        }
+
+        raiseEvent(FundsWithdrawEvent.builder()
+                .id(this.id)
+                .amount(amount)
+                .build());
+    }
+
+    public void apply(FundsWithdrawEvent event) {
+        this.id = event.getId();
+        this.balance -= event.getAmount();
+    }
+
+    public void closeAccount() {
+        if (!this.active) {
+            throw new IllegalStateException("Account is already closed");
+        }
+        raiseEvent(AccountClosedEvent.builder()
+                .id(this.id)
+                .build());
+    }
+
+    public void apply(AccountClosedEvent event) {
+        this.id = event.getId();
+        this.active = false;
+    }
+}
