f8f03eac1a650fd9bdb756deea4af9188ee560f0
José Robson <joserobsonsiqueira23@hotmail.com>
2024-05-21 15:02:54 -0300

    feat: WithdrawFundsController created

---
+++ bank-account/account.cmd/src/main/java/io/github/robinhosz/techbank/account/cmd/api/controllers/WithdrawFundsController.java
@@ -0,0 +1,45 @@
+package io.github.robinhosz.techbank.account.cmd.api.controllers;
+
+import io.github.robinhosz.techbank.account.cmd.api.commands.DepositFundsCommand;
+import io.github.robinhosz.techbank.account.cmd.api.commands.WithdrawFundsCommand;
+import io.github.robinhosz.techbank.account.cmd.exceptions.AggregateNotFoundException;
+import io.github.robinhosz.techbank.account.common.dto.BaseResponse;
+import io.github.robinhosz.techbank.cqrs.core.infrastructure.CommandDispatcher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.text.MessageFormat;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@RestController
+@RequestMapping("/api/v1/withdrawFunds")
+public class WithdrawFundsController {
+    private final Logger logger = Logger.getLogger(OpenAccountController.class.getName());
+
+    @Autowired
+    private CommandDispatcher commandDispatcher;
+
+    @PutMapping("/{id}")
+    public ResponseEntity<BaseResponse> withdrawFunds(@PathVariable(value = "id") String id, @RequestBody WithdrawFundsCommand command) {
+
+        try {
+            command.setId(id);
+            commandDispatcher.send(command);
+            return new ResponseEntity<>(new BaseResponse("Withdraw Funds deposited successfully"), HttpStatus.OK);
+
+        } catch (IllegalStateException | AggregateNotFoundException e) {
+            logger.log(Level.WARNING, MessageFormat.format("Client made a had request but failed to process. Reason: {0}", e.toString()));
+            return new ResponseEntity<>(new BaseResponse(e.toString()), HttpStatus.BAD_REQUEST);
+
+        } catch (Exception e) {
+            var safeErrorMessage = MessageFormat.format("Error while processing request to withdraw funds from bank account. Reason: {0}", e.toString());
+            logger.log(Level.SEVERE, safeErrorMessage, e);
+            return new ResponseEntity<>(new BaseResponse(safeErrorMessage), HttpStatus.INTERNAL_SERVER_ERROR);
+
+        }
+    }
+
+}
