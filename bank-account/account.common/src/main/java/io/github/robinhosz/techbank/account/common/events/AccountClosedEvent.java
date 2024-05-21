51928df53311cabb609acc2fdfc88d3f3c49cb6c
José Robson <joserobsonsiqueira23@hotmail.com>
2024-05-21 15:24:37 -0300

    fix: deserializer kafka no args constructor error

--- bank-account/account.common/src/main/java/io/github/robinhosz/techbank/account/common/events/AccountClosedEvent.java
+++ bank-account/account.common/src/main/java/io/github/robinhosz/techbank/account/common/events/AccountClosedEvent.java
@@ -2,9 +2,11 @@ package io.github.robinhosz.techbank.account.common.events;
 
 import io.github.robinhosz.techbank.cqrs.core.events.BaseEvent;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Data
+@NoArgsConstructor
 @SuperBuilder
 public class AccountClosedEvent extends BaseEvent {
 }
